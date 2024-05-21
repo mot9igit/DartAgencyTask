5319b4c425f4eca01652a4d7c774758d0a052f14
Daniil <baldindaniilza@mail.ru>
2024-05-21 18:24:51 +0500

    Добавление теней компоненту кнопки

--- src/components/UI/CustomButton.vue
+++ src/components/UI/CustomButton.vue
@@ -43,6 +43,9 @@ export default defineComponent({
 	position: relative;
 	overflow: hidden;
 
+	box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 3px 1px 0 rgba(0, 0, 0, 0.12),
+		0 2px 2px 0 rgba(0, 0, 0, 0.14);
+
 	&--red {
 		background-color: var(--color-red);
 		color: var(--color-white);
