89f28fec25c02a79b68ac3eefb8f1421e9baf2cd
Daniil <baldindaniilza@mail.ru>
2024-06-07 10:47:29 +0500

    Фикс ошибки

--- src/components/UI/NdsSelect/NdsSelectItem.vue
+++ src/components/UI/NdsSelect/NdsSelectItem.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import NdsType from "./NdsSelect.vue";
+import { NdsType } from "./NdsSelect.vue";
 
 export default defineComponent({
 	name: "NdsSelectItem",
